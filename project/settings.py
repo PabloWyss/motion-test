b4ab3105d7a37117a62ba5115cb3fbee5ef5b202
Pablo Wyss <pabloawyss@gmail.com>
2023-03-17 11:02:14 +0100

    added apps

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'post',
+    'registrationprofile',
+    'user',
+
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
