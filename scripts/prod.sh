06ea132d9ad4572dcbdc58474239d22a5f58ccac
Pablo Wyss <pabloawyss@gmail.com>
2024-02-14 16:36:08 +0100

    bell alerts added and user request changed

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -1,4 +1,4 @@
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate
 gunicorn -w 4 -b 0.0.0.0:8000 project.wsgi:application
 
